e6e15ac2af2bb99a875d8c948be902f8760e45dd
Zman840 <Zman840@gmail.com>
2016-05-11 08:45:22 -0500

    Updated files file modules
    line endings are all LF for Linux, made sure events are correct, and
    main is rewritten to what Nath's files were

--- DiscordBot.js
+++ DiscordBot.js
@@ -1,51 +1,32 @@
-// =========================================================
-// Discord Bot Project
-
-console.log("> Discord Bot Project");
+console.log(`> Discord Bot Project`);
 console.log(`> Node.js version: ${process.version}`);
-console.log("> ");
+console.log(`> `);
 
 
 // =========================================================
-// Loading Discord.js
-console.log("> Loading discord.js");
-
-// load core variables outside block. Disregard initialization on declaration
+// load core variables outside block.
+// Disregard initialization on declaration
 var discord;
 var authDetails;
 
 try {
-  discord = require("discord.js");
+  console.log(`> Loading files:`);
+
+  discord = require(`discord.js`);
+  console.log(`> Loaded 'discord.js'`);
+
+  authDetails = require(`./DiscordBot-AuthDetails.json`);
+  console.log(`> Loaded 'DiscordBot-AuthDetails.json'`);
+
+  config = require(`./DiscordBot-Config.json`);
+  console.log(`> Loaded 'DiscordBot-AuthDetails.json'`);
+
+  console.log(`> `);
 } catch (e) {
-  console.log("> ");
-  console.log("> ERROR: discord.js failed to load");
-  console.log(`> ${e}`);
-  process.exit();
-}
-
-
-// =========================================================
-// Loading authentication details
-console.log("> Loading authentication details");
-try {
-  authDetails = require("./DiscordBot-Auth.json");
-} catch (e) {
-  console.log("> ");
-  console.log("> ERROR: Authentication details failed to load");
-  console.log(`> ${e}`);
-  process.exit();
-}
-
-
-// =========================================================
-// Loading configurations
-console.log("> Loading configurations");
-try {
-  config = require("./DiscordBot-Config.json");
-} catch (e) {
-  console.log("> ");
-  console.log("> ERROR: Configuration failed to load!");
-  console.log(`> ${e}`);
+  console.log(`> `);
+  console.log(`> Something went wrong!`);
+  console.log(`> `);
+  console.log(`> ${e.stack}`);
   process.exit();
 }
 
@@ -54,53 +35,32 @@ try {
 // Load discord client
 bot = new discord.Client();
 
-
 // =========================================================
-// When the bot is ready
-bot.on("ready", () => {
-  console.log("> Connected");
-  console.log("> ");
-  console.log("> ======================= BOT SETTINGS ===");
-  console.log(`>             BOT ID | ${bot.user.id}`);
-  console.log(`>       BOT OWNER ID | ${config.botOwnerID}`);
-  console.log(`>  SERVERS CONNECTED | ${bot.servers.length}`);
-  console.log(">                    | ");
-  console.log(`>            ENABLED | ${config.isEnabled}`);
-  console.log(">                    | ");
-  console.log("> ==================== SERVER SETTINGS ===");
-  console.log(`>      SYSTEM PREFIX | ${config.commandPrefixSystem}`);
-  console.log(`>      NORMAL PREFIX | ${config.commandPrefixNormal}`);
-  console.log(">                    | ");
-  console.log(`>      ADMIN ROLE ID | ${config.roleIDAdministrator}`);
-  console.log(`>        MOD ROLE ID | ${config.roleIDModerator}`);
-  console.log(`> RESTRICTED ROLE ID | ${config.roleIDRestricted}`);
-  console.log("> ========================================");
-  console.log("> ");
-  bot.setStatus("online", `${config.commandPrefixNormal}help`);
-});
+// Load event files
+try {
+  // When the bot is ready.
+  require(`./events/ready.js`);
+  console.log(`> Event file 'ready.js' loaded.`);
 
+  // When the bot gets disconnected.
+  require(`./events/disconnected.js`);
+  console.log(`> Event file 'disconnected.js' loaded.`);
 
-// =========================================================
-// When the bot gets disconnected
-bot.on("disconnected", () => {
-  console.log("> ");
-  console.log("> Disconnected!");
-  console.log("");
-  process.exit(1);
-});
+  // When the bot receives a message.
+  require(`./events/message.js`);
+  console.log(`> Event file 'message.js' loaded.`);
+} catch (e) {
+  console.log(`> `);
+  console.log(`> Something went wrong!`);
+  console.log(`> `);
+  console.log(`> ${e.stack}`);
+  process.exit();
+}
 
-// =========================================================
-// Separate message events list into a seperate folder
-require("./events/message.js");
 
 // =========================================================
 // Authentication
-console.log("> ");
-console.log("> Connecting...");
-if (!authDetails.token) {
-  bot.login(authDetails.username, authDetails.password, (error) => {
-    console.log(`> Failed: ${error}`);
-  });
-} else {
-  bot.loginWithToken(authDetails.token);
-}
+console.log(`> `);
+console.log(`> Connecting...`);
+// bot.login(authDetails.username, authDetails.password)
+bot.loginWithToken(authDetails.token);
